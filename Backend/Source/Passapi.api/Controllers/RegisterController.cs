da068c9d2ce13235bad293fd0c9032979de89d79
vitalibeniamino <38581963+vitalibeniamino@users.noreply.github.com>
2020-11-23 23:53:56 +0100

    Added startcode
    Created startfiles
    new file:   Backend/Source/Passapi.api/Controllers/LoginController.cs
    new file:   Backend/Source/Passapi.api/Controllers/RegisterController.cs
    new file:   Backend/Source/Passapi.api/Models/RegisterModel.cs
    new file:   Backend/Source/Passapi.api/Passapi.api.csproj
    new file:   Backend/Source/Passapi.api/Program.cs
    new file:   Backend/Source/Passapi.api/Properties/launchSettings.json
    new file:   Backend/Source/Passapi.api/Startup.cs
    new file:   Backend/Source/Passapi.api/appsettings.Development.json
    new file:   Backend/Source/Passapi.api/appsettings.json
    new file:   Backend/Source/Passapi.sln
    
    Beniamino

---
+++ Backend/Source/Passapi.api/Controllers/RegisterController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Passapi.api.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Passapi.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        // GET: api/<RegisterController>
+        /*[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }*/
+
+        // GET api/<RegisterController>/5
+        /*[HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }*/
+
+        // POST api/<RegisterController>
+        [HttpPost("register")]
+        [AllowAnonymous]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new User
+            {
+
+            };
+
+            IdentityUser
+        }
+
+        // PUT api/<RegisterController>/5
+        /*[HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }*/
+
+        // DELETE api/<RegisterController>/5
+        /*[HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
---
+++ Backend/Source/Passapi.api/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Passapi.api.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(12)]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
